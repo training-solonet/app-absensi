fcfb15ad27795a27cdd7c47da836c1fce292a8f5
felandraaaa <felandra411@gmail.com>
2024-01-29 15:36:25 +0700

    menambahkan logo pada sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,9 +4,8 @@
           <div class="app-brand demo">
             <a href="index.html" class="app-brand-link">
               <span class="app-brand-logo demo">
-              <img src="{{ asset('assets/img/logo.png') }} ">
+              <img style="width: 175px; height: 80px;" src="{{ asset('assets/img/logo.png') }} ">
               </span>
-              <span class="app-brand-text demo menu-text fw-bolder ms-2">cc</span>
             </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
